a6513d4cb041b062442cf4156fb8d5bf163595d5
jeanpandolfi <jean-pandolfi@hotmail.com>
2020-04-02 13:51:54 -0300

    Definindo regra de negocio pra os componentes painel e progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,10 +11,49 @@ import { FRASES } from './frases-mock'
 export class PainelComponent implements OnInit {
 
   private frases: Array<Frase> = FRASES;
+  private instrucao: string = "Traduza a frase:";
+  private resposta: string = "";
+  private rodada: number = 0;
+  private rodadaFrase: Frase;
+  public progresso: number = 0;
 
-  constructor() { console.log(this.frases)}
+  constructor() { 
+    this.atualizaRodada(); 
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * atualizaResposta
+   */
+  public atualizarResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+  }
+
+  /**
+   * verificarResposta
+   */
+  public verificarResposta(): void {
+
+    console.log(this.resposta);
+    console.log(this.rodadaFrase.frasePtBr);
+
+    if(this.rodadaFrase.frasePtBr.toLowerCase == this.resposta.toLowerCase){
+        alert("A tradução está corrreta");
+        this.rodada++;
+        this.atualizaRodada();
+        this.progresso += (100 / this.frases.length);
+       
+
+    }else{
+      alert("A tradução está Errada");
+    }
+    
+  }
+
+  private atualizaRodada(): void {
+    this.rodadaFrase = this.frases[this.rodada];
+    this.resposta = "";
+  }
 }
